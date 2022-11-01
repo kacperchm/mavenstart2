1e71f8aa05832a2addc91b0d2ed8920315113890
kacperchm <chmuraka1998@gmail.com>
2022-11-01 23:07:09 +0100

    Initial commit

---
+++ src/main/java/io/github/mat3e/Lang.java
@@ -0,0 +1,47 @@
+package io.github.mat3e;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "languages")
+class Lang {
+    @Id
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    private Integer id;
+    private String welcomeMsg;
+    private String code;
+
+    public Lang() {
+    }
+
+    public Lang(Integer id, String welcomeMsg, String code) {
+        this.id = id;
+        this.welcomeMsg = welcomeMsg;
+        this.code = code;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -0,0 +1,84 @@
+package io.github.mat3e;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HelloServiceTest {
+    private final static String WELCOME = "Hello";
+    private final static String FALLBACK_ID_WELCOME = "Hola";;
+
+    @Test
+    public void test_nullName_prepareGreeting_returnsGreetingWithFallback() {
+
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "-1");
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_name_returnsGreetingWithName() {
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository);
+        var name = "test";
+        var result = SUT.prepareGreeting(name, "-1");
+        assertEquals( WELCOME + " " + name + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_nonExistingLang_returnsGreetingWithFallbackLang() {
+        var mockRepository = new LangRepository() {
+            @Override
+            Optional<Lang> findById(Integer id) {
+                return Optional.empty();
+            }
+        };
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "-1");
+        assertEquals( HelloService.FALLBACK_LANG.getWelcomeMsg() + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_nullLang_returnsGreetingWithFallbackIdLang() {
+
+        var mockRepository = fallbackLangIdRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, null);
+        assertEquals( FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_textLang_returnsGreetingWithFallbackIdLang() {
+
+        var mockRepository = fallbackLangIdRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "abc");
+        assertEquals( FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Integer id) {
+                if(id.equals(HelloService.FALLBACK_LANG.getId())) {
+                    return Optional.of(new Lang(null,FALLBACK_ID_WELCOME, null));
+                }
+                return Optional.empty();
+            }
+        };
+    }
+
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Integer id) {
+                return Optional.of(new Lang(null, WELCOME, null));
+            }
+        };
+    }
+}
